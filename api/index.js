dee3978ac26489c4df08b9cce3ea8e2b74290440
Samer Bejjani <samerbejjani@macbook-pro.home>
2024-02-23 15:23:28 -0500

    Initial commit

--- api/index.js
+++ api/index.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = require('express')();
+const { v4 } = require('uuid');
 
-app.get('/', (req, res) => {
-  res.send('Hello World from Express!');
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
 });
+
+module.exports = app;
